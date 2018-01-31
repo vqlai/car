80d65c1d1fd2ab2b5be35efb4d97f2f048a80e3e
vqlai <695850246@qq.com>
2018-01-31 10:56:04 +0800

    修改页面布局

---
+++ 20180131/src/pages/demo1.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="demo1">
+		<!-- <top-header></top-header> -->
+		demo1
+	</section>
+</template>
+
+<script>
+export default {
+  name: 'c-demo1',
+  data () {
+    return {
+    	
+    }
+  },
+  mounted() {
+  },
+  methods:{
+  },
+  components:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	
+</style>
